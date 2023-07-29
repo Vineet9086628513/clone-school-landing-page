765362d5c74fc0ef0f58a1ea1db6b2886254eb67
Sunil Kumar Yadav <123sunilkr@gmail.com>
2023-07-29 16:12:12 +0530

    fix: icon change on click

--- html/main.js
+++ html/main.js
@@ -1,46 +1,38 @@
- //change a navbar scrollba
+//change a navbar scrollba
 
-window.addEventListener('scroll', () =>{
-    document.querySelector('nav').classList.toggle
-    ('window-scroll',window.scrollY>100)
-})
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 100);
+});
 
+const faqs = document.querySelectorAll(".faq");
 
-
-
-
-const faqs = document.querySelectorAll('.faq');
-
-faqs.forEach(faq => {
-    faq.addEventListener('click',() =>{
-    faq.classList.toggle('open');
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
 
     //change icon
-    const icon = faq.querySelector('.faq_icon i');
-    if(icon.className === 'expand_more'){
-        icon.className = "expand_more"    
-    }
-})
-})
-
-
-
+    const icon = faq.querySelector(".faq_icon i");
+    icon.className =
+      icon.className === "fa fa-plus" ? "fa fa-minus" : "fa fa-plus";
+  });
+});
 
 //show /hide nav menu
 const menu = document.querySelector(".nav_menu");
 const menubtn = document.querySelector(" #open-menu-btn");
 const closebtn = document.querySelector(" #close-menu-btn");
 
-menubtn.addEventListener('click', () =>{
-    menu.style.display = "flex";
-    closebtn.style.display = "inline-block";
-    menubtn.style.display ="none";
-})
+menubtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closebtn.style.display = "inline-block";
+  menubtn.style.display = "none";
+});
 
-const closeNav = () =>{
-    menu.style.display = "none";
-    closebtn.style.display = "none";
-    menubtn.style.display ="inline-block";
-
-}
-closebtn.addEventListener('click', closeNav)
+const closeNav = () => {
+  menu.style.display = "none";
+  closebtn.style.display = "none";
+  menubtn.style.display = "inline-block";
+};
+closebtn.addEventListener("click", closeNav);
